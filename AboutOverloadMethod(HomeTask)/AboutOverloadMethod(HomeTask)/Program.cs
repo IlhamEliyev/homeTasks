5b6ac131beee9d491509b586b2e29c00f8abac65
IlhamEliyev <2srsxdl@code.edu.az>
2022-03-15 03:37:15 +0400

    OverloadMethod

---
+++ AboutOverloadMethod(HomeTask)/AboutOverloadMethod(HomeTask)/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace AboutOverloadMethod_HomeTask_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(CalculateArea(2,5,5));
+        }
+
+        static int CalculateArea(int r)
+        {
+            int area = 3 * r * r;
+            return area;
+        }
+        static int CalculateArea(int a, int b)
+        {
+            int area = a * b;
+            return area;
+        }
+
+        static int CalculateArea(int a, int b, int c)
+
+        {
+            int area = 2 * (a * b + a * c + b * c);
+            return area;
+        }
+    }
+}
---
+++ ConsoleApp3/ConsoleApp3/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ////Task 1
+            //Console.WriteLine("Zehmet olmasa ededi daxil edin: ");
+            //int n = Convert.ToInt32(Console.ReadLine());
+            //bool isReminder = false;
+            //for (int i = 2; i < n/2; i++)
+            //{
+            //    if (n%i == 0 )
+            //    {
+            //        isReminder = true;
+            //    }
+            //    else
+            //    {
+            //        isReminder = false;
+            //    }
+            //}
+            //if (isReminder) { Console.WriteLine("Eded sadedir"); }
+            //else { Console.WriteLine("Eded murekkebdir "); }
+
+            ////Task 2
+            //Console.WriteLine("Zehmet olmasa ededi daxil edin: ");
+            //int n = Convert.ToInt32(Console.ReadLine());
+            //int count = 1;
+            //if (n < 0) n = n * (-1);
+            //while (n > 9)
+            //{
+            //    n = n / 10;
+            //    count++;
+            //}
+            //Console.WriteLine(count);
+
+
+            ////Task 3
+            //Console.WriteLine("Zehmet olmasa ededi daxil edin: ");
+            //int num = Convert.ToInt32(Console.ReadLine());
+            //int changeNum = num;
+            //int reminder;
+            //int sum = 0;
+
+            //while (changeNum > 0)
+            //{
+            //    reminder = changeNum % 10;
+            //    sum = sum * 10 + reminder;
+            //    changeNum = changeNum / 10;
+            //}
+            //if (sum == num)
+            //{
+            //    Console.WriteLine("Palindromdur");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Polindrom deyil");
+            //}
+
+        }
+    }
+}
